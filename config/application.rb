f358fa428f89899e3480ae65c373c8f75ea68c4f
KishiKyousuke <carp011808pg@icloud.com>
2020-07-13 23:47:31 +0900

    rubocopに通してコードを整理

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,4 +19,3 @@ module BooksApp
     # the framework and any gems in your application.
   end
 end
-

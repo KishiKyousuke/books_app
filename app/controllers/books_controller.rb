3d2b3ecdf43c5ca67b07369c0dc2a90da6e3a025
KishiKyousuke <carp011808pg@icloud.com>
2020-07-25 01:46:46 +0900

    不要なファイルの削除/indexアクションの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = current_user.books
+    @books = current_user.books.page(params[:page])
   end
 
   # GET /books/1

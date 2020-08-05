a01185795caacdc89b9c8b3e04f5e39131d27238
KishiKyousuke <carp011808pg@icloud.com>
2020-08-05 16:08:58 +0900

    omniauthの実装

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development? || Rails.env.test?
-    provider :github, ENV["GITHUB_ID"], ENV["GITHUB_SECRET"]
-  end
-end

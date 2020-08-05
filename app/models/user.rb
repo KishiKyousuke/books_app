250d278bcba67f0fe9e3e1f6ec5075f0107f8b90
KishiKyousuke <carp011808pg@icloud.com>
2020-08-05 20:54:09 +0900

    GitHubでのログイン機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
+
+  def self.find_for_github_oauth(auth, signed_in_resource = nil)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 end

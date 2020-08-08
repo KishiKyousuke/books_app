6392727becdaca0caf686180ed8ecf75a755af1c
KishiKyousuke <carp011808pg@icloud.com>
2020-08-08 16:30:49 +0900

    Relationshipモデルの追加、routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations" }
-  get "/users/:id", to: "users#show"
-  get "/users", to: "users#index"
+  resources :users, only: [:index, :show]
   root to: "books#index"
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_161635) do
+ActiveRecord::Schema.define(version: 2020_08_08_071119) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,6 +42,13 @@ ActiveRecord::Schema.define(version: 2020_08_06_161635) do
     t.string "picture"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

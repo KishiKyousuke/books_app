71299abd91436c92ec60c4c6dd80297c9397c45a
KishiKyousuke <carp011808pg@icloud.com>
2020-08-09 21:24:15 +0900

    rubocopに通す

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save!
-    redirect_to users_path, notice: t('view.relationship.flash.follow')
+    redirect_to users_path, notice: t("view.relationship.flash.follow")
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy!
-    redirect_to users_path, notice: t('view.relationship.flash.unfollow')
+    redirect_to users_path, notice: t("view.relationship.flash.unfollow")
   end
 end
